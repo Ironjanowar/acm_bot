317e07b98b9d102faabf73771e0132211ac447bb
Ironjanowar <ironjanowar@gmail.com>
2017-05-25 01:06:20 +0200

    Comments removed

--- lib/bot/bot.ex
+++ lib/bot/bot.ex
@@ -11,36 +11,6 @@ defmodule AcmBot.Bot do
     answer msg, "Hola! Soy el bot de <b>ACM UPM</b> 😃\nUsa /help para ver que puedo hacer.", bot: name, parse_mode: "HTML"
   end
 
-  def handle({:command, "register", msg}, name, _) do
-    force_reply = %Telex.Model.ForceReply{force_reply: true}
-    # No se como guardar el message_id de este a la que sale
-    answer msg, "Dime la descripcion de este grupo", bot: name, reply_markup: force_reply
-  end
-
-  # def handle({:command, "registergroup", %{text: extra, chat: %{id: telegram_id, title: group_name}} = msg}, name, _) do
-  #   data =
-  #     String.split(extra, " ")
-  #     |> List.to_tuple
-  #     |> (fn x -> Enum.into([x], %{}) end).()
-
-  #   case data do
-  #     %{link: link, descrition: description} ->
-  #       Acm.Upm.insert %Schemas.Group{telegram_id: telegram_id, name: group_name, description: description, link: link}
-  #     %{link: link} ->
-  #       Acm.Upm.insert %Schemas.Group{telegram_id: telegram_id, name: group_name, link: link}
-  #     %{descrition: description} ->
-  #       Acm.Upm.insert %Schemas.Group{telegram_id: telegram_id, name: group_name, description: description}
-  #     _ ->
-  #       Acm.Upm.insert %Schemas.Group{telegram_id: telegram_id, name: group_name}
-  #   end
-
-  #   Logger.info "Group #{group_name} [#{telegram_id}] registered"
-  # end
-
-  # def handle({:regex, "/(description:).+/i", _msg}, _name, _) do
-  #   Logger.debug "Matchedd"
-  # end
-
   def handle({:command, "registergroup", %{from: %{id: uid}} = msg}, name, _) do
     Step.add_user(uid)
     markup = %Telex.Model.ForceReply{force_reply: true}
